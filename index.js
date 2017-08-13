5a86a15570a24310729e437a5700a4b1e4720466
Evgeny Evseev <pelid80@gmail.com>
2017-08-13 11:03:16 +0300

    adds support for visibility API

--- index.js
+++ index.js
@@ -1,33 +1,110 @@
 var TIMEOUT_IN_SECS = 3 * 60
-var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>'
-
-// adds HTML tag to current page
-var timerContainer = document.createElement('div')
-timerContainer.setAttribute("style", "height: 100px;")
-var bodyTag = document.body
-bodyTag.insertBefore(timerContainer, bodyTag.firstChild)
-timerContainer.innerHTML = TEMPLATE
-
-function getTimestampInSecs(){
-  var timestampInMilliseconds = new Date().getTime()
-  return Math.round(timestampInMilliseconds/1000)
-}
+var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
 }
 
-var timestampOnStart = getTimestampInSecs()
-
-function displayTimer(){
-  var currentTimestamp = getTimestampInSecs()
-  var secsGone = currentTimestamp - timestampOnStart
-  var secsLeft = Math.max(TIMEOUT_IN_SECS - secsGone, 0)
-
-  var minutes = Math.floor(secsLeft / 60);
-  var seconds = secsLeft - minutes * 60;
-
-  document.getElementById('timer-minutes').innerHTML = padZero(minutes)
-  document.getElementById('timer-seconds').innerHTML = padZero(seconds)
+class Timer{
+  // IE does not support new style classes yet
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+  constructor(timeout_in_secs){
+    this.isRunning = false
+    this.timestampOnStart = null
+    this.initial_timeout_in_secs = timeout_in_secs
+    this.timeout_in_secs = this.initial_timeout_in_secs
+  }
+  getTimestampInSecs(){
+    var timestampInMilliseconds = new Date().getTime()
+    return Math.round(timestampInMilliseconds/1000)
+  }
+  start(){
+    if (this.isRunning)
+      return
+    this.timestampOnStart = this.getTimestampInSecs()
+    this.isRunning = true
+  }
+  stop(){
+    if (!this.isRunning)
+      return
+    this.timeout_in_secs = this.calculateSecsLeft()
+    this.timestampOnStart = null
+    this.isRunning = false
+  }
+  calculateSecsLeft(){
+    if (!this.isRunning)
+      return this.timeout_in_secs
+    var currentTimestamp = this.getTimestampInSecs()
+    var secsGone = currentTimestamp - this.timestampOnStart
+    return Math.max(this.timeout_in_secs - secsGone, 0)
+  }
 }
-setInterval(displayTimer, 300)
+
+class TimerWidget{
+  // IE does not support new style classes yet
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+  construct(){
+    this.timerContainer = this.minutes_element = this.seconds_element = null
+  }
+  mount(rootTag){
+    if (this.timerContainer)
+      this.unmount()
+
+    // adds HTML tag to current page
+    this.timerContainer = document.createElement('div')
+
+    this.timerContainer.setAttribute("style", "height: 100px;")
+    this.timerContainer.innerHTML = TEMPLATE
+
+    rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
+
+    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
+    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
+  }
+  update(secsLeft){
+    var minutes = Math.floor(secsLeft / 60);
+    var seconds = secsLeft - minutes * 60;
+
+    this.minutes_element.innerHTML = padZero(minutes)
+    this.seconds_element.innerHTML = padZero(seconds)
+  }
+  unmount(){
+    if (!this.timerContainer)
+      return
+    this.timerContainer.remove()
+    this.timerContainer = this.minutes_element = this.seconds_element = null
+  }
+}
+
+
+function main(){
+
+  var timer = new Timer(TIMEOUT_IN_SECS)
+  var timerWiget = new TimerWidget()
+  var intervalId = null
+
+  timerWiget.mount(document.body)
+
+  function handleIntervalTick(){
+    var secsLeft = timer.calculateSecsLeft()
+    timerWiget.update(secsLeft)
+  }
+
+  function handleVisibilityChange(){
+    if (document.hidden) {
+      timer.stop()
+      clearInterval(intervalId)
+      intervalId = null
+    } else {
+      timer.start()
+      intervalId = intervalId || setInterval(handleIntervalTick, 300)
+    }
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+  document.addEventListener("visibilitychange", handleVisibilityChange, false);
+  handleVisibilityChange()
+}
+
+// initialize timer when page ready for presentation
+window.addEventListener('load', main)
